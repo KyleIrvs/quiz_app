38bbe61f38de55612c6bfa34c6fa2dcaced474d7
Kyle Irvine <kyle.irvine98@gmail.com>
2023-05-31 08:56:35 +0200

    add functionality to display single question

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -10,17 +13,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Q text'),
+          Text(
+            currentQuestion.text,
+            style: TextStyle(color: Colors.white),
+          ),
           const SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: const Text('A1 text')),
-          ElevatedButton(onPressed: () {}, child: const Text('A2 text')),
-          ElevatedButton(onPressed: () {}, child: const Text('A3 text')),
-          ElevatedButton(onPressed: () {}, child: const Text('A3 text')),
+          // ... spreads iterables to individual widgets instead of List<widgets> 
+          ...currentQuestion.answers.map((answerText) {
+            return AnswerButton(answerText, (){});
+          })
         ],
       ),
     );
